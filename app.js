15c6910721662c946852418a42816d188fc2eb1f
Blackstars <nelson.64@live.fr>
2023-11-27 14:44:53 +0100

    depart quete express

---
+++ app.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const { get } = require("express/lib/response");
+
+const app = express();
+
+const port = 5000;
+
+const movies = [
+  {
+    id: 1,
+    title: "Citizen Kane",
+    director: "Orson Wells",
+    year: "1941",
+    color: false,
+    duration: 120,
+  },
+  {
+    id: 2,
+    title: "The Godfather",
+    director: "Francis Ford Coppola",
+    year: "1972",
+    color: true,
+    duration: 180,
+  },
+  {
+    id: 3,
+    title: "Pulp Fiction",
+    director: "Quentin Tarantino",
+    year: "1994",
+    color: true,
+    duration: 180,
+  },
+];
+
+// ----------------debut de l'exercice---------------
+app.get("/", (req, res) => {
+  res.send("Welcome to my favourite movie list");
+});
+
+app.get("/api/movies", (req, res) => {
+  res.status(200).json(movies);
+});
+
+const film = (req, res) => {
+  const id = parseInt(req.params.id);
+  let idFilm = "";
+
+  for (let i = 0; i < movies.length; i++) {
+    if (movies[i].id === id) {
+      idFilm = movies[i];
+    }
+  }
+  if (idFilm) {
+    res.status(200).json(idFilm);
+  } else {
+    res.send("Not Found");
+  }
+};
+
+app.get("/api/movies/:id", film);
+
+// --------------------------fin de l'exercice-----------------------
+
+app.get("/index", (req, res) => {
+  res.send("je suis dans l'index");
+});
+
+const WelcomeName = (req, res) => {
+  res.send(`Welcome ${req.params.name}`);
+};
+
+app.get("/users/:name", WelcomeName);
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
